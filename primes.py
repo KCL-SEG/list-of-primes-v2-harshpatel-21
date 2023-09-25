4a4e3aea88db167df59d491b97f8814803c0a27c
harshpatel-21 <patel.harshraj2004@gmail.com>
2023-09-25 15:06:36 +0100

    solution

--- primes.py
+++ primes.py
@@ -2,5 +2,21 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    if number_of_primes <= 0:
+        raise ValueError("Number of primes must be positive")
+    
+    primes_list = []
+    count = 0
+    i=2
+    while count<number_of_primes:
+        if is_prime(i):
+            primes_list += [i]
+            count+=1
+        i+=1
+    return primes_list
+        
+def is_prime(n):
+    return all(n%i for i in range(2,int((n**0.5)+1)))
+
+
+print(primes(10))
